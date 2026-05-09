9165c251401456b0af9799e6ab2f91701691ca37
gelila@0913 <gelilasintayehu79@gmail.com>
2026-05-10 01:16:33 +0300

    enlarging the logo in some pages

--- hisabapp/lib/features/Auth/login/login.dart
+++ hisabapp/lib/features/Auth/login/login.dart
@@ -32,7 +32,7 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Image.asset('assets/images/logo1.jpg', height: 70, fit: BoxFit.contain),
+              Image.asset('assets/images/logo1.jpg', height: 100, fit: BoxFit.contain),
               const SizedBox(height: 12),
               const Text('Log in', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
               const SizedBox(height: 6),
